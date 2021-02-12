86bb4794e4e5a47817b8ed8bc5b94848b9aa7a07
Omar Yehia <omaryahiatawfiek@gmail.com>
2021-02-13 01:01:53 +0200

    Another updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:csec_app/splashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,12 +9,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Map<int, Color> color = {
+      50: Color.fromRGBO(117, 182, 196, .1),
+      100: Color.fromRGBO(117, 182, 196, .2),
+      200: Color.fromRGBO(117, 182, 196, .3),
+      300: Color.fromRGBO(117, 182, 196, .4),
+      400: Color.fromRGBO(117, 182, 196, .5),
+      500: Color.fromRGBO(117, 182, 196, .6),
+      600: Color.fromRGBO(117, 182, 196, .7),
+      700: Color.fromRGBO(117, 182, 196, .8),
+      800: Color.fromRGBO(117, 182, 196, .9),
+      900: Color.fromRGBO(117, 182, 196, 1),
+    };
+    MaterialColor mainColor = MaterialColor(0xFF880E4F, color);
+
     return MaterialApp(
       title: 'CSEC',
       theme: ThemeData(
+        primarySwatch: mainColor,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: MySplashScreen(),
     );
   }
 }
